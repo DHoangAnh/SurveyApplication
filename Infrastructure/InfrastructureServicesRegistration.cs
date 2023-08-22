3d3e74d37956e0775271b1cdb69410eadd316583
dinhhoanganh2020@gmail.com <dinhhoanganh2020@gmail.com>
2023-08-22 09:22:19 +0700

    Add project files.

---
+++ Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyApplication.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            return services;
+        }
+    }
+}
---
+++ SurveyApplication.Persistence/SurveyApplicationDbContextFactor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyApplication.Persistence
+{
+    public class SurveyApplicationDbContextFactor : IDesignTimeDbContextFactory<SurveyApplicationDbContext>
+    {
+        public SurveyApplicationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            var builder = new DbContextOptionsBuilder<SurveyApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new SurveyApplicationDbContext(builder.Options);
+        }
+    }
+}
